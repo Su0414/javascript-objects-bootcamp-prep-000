fa4ed00e3758220908fac3ec9fb046cfbf578432
Susan <susanaik@gmail.com>
2018-04-17 14:50:08 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,6 +10,7 @@ function updatePlaylist(playlist, artistName, songTitle){
   return playlist;
 }
 
-function removeFromPlaylist(artistName){
-  
+function removeFromPlaylist(playlist, artistName){
+  delete playlist.artistName;
+  return playlist;
 }
