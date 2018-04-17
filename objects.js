adf8a1783ccb919ea0b3c87d9fb7a249d84f7402
Susan <susanaik@gmail.com>
2018-04-17 14:42:34 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ const playlist = {
 function updatePlaylist(playlist, artistName, songTitle){
   console.log(playlist);
   const newPlay = Object.assign( {} , playlist, {[artistName]:songTitle});
-  console.log(playlist.artistName);
+  console.log(newPlay.artistName);
   return newPlay;
 }
